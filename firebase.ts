b0fdea854c35a58421b3ca98afb9343573a757f4
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-08-01 23:54:05 +0500

    Firebase Configured

---
+++ firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "@firebase/app";
+import { getAnalytics } from "@firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIRE_BASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIRE_BASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIRE_BASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIRE_BASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIRE_BASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIRE_BASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIRE_BASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const fireStore=getFirestore(app);
+
+module.exports={
+  fireStore,
+  analytics,
+}
