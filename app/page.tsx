c95c7e4fe95eee7e655dbe61a9ca4debebe3eca0
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-08-03 23:58:45 +0500

    View dashboard button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 
 const Home = () => {
   return (
@@ -7,7 +7,7 @@ const Home = () => {
       <Typography>My Pantry Tracker®</Typography>
       <Typography>Download My Pantry Tracker now for free to keep an organized inventory of your pantry!</Typography>
       <Typography>Automatically add items manually or with your device’s camera!</Typography>
-      
+      <Button>View Dashboard</Button>
     </Box>
   )
 }
