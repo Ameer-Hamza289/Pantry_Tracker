2a94013451c988b92832ab219ef42dc2c0d014b1
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-08-04 12:24:35 +0500

    Back to dashboard button added and UI updated

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,17 +1,24 @@
+'use client'
 import MasonryLayout from '@/components/Masonary'
 import { Box, Button, Grid, TextField, Typography } from '@mui/material'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Inventory = () => {
+  const router=useRouter();
   return (
     <Box p={4} >
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
       <Typography variant='h3' fontWeight={700}>Inventory Section</Typography>
+      <Button variant='outlined' onClick={()=>router.back()}>Back to Dashboard</Button>
+
+      </Box>
       <Grid container spacing={3} alignItems='center' marginY={3}>
         <Grid item xs={9}>
           <TextField variant='outlined' placeholder='Search inventory' sx={{ flex: 1, width: "100%" }} />
         </Grid>
         <Grid item xs={3}>
-          <Button variant='outlined' fullWidth sx={{ paddingY: 1.7 }}>Add new Item</Button>
+          <Button variant='contained' fullWidth sx={{ paddingY: 1.8 }}>Add new Item</Button>
         </Grid>
       </Grid>
       <MasonryLayout />
